8605a49f0160bb382c6f9ed1540f1d4237d6c224
F6323258 Lucas Pereira Gomes Linard <lucaslinard@bb.com.br>
2024-06-18 09:27:08 -0300

    Marcelo

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/add/?num1=<num1>&num2=<num2>")
-async def say_hello(name: str):
-    num1 = int()
-    return {"message": f"Hello {name}"}
+@app.get("/add/{num1}/{num2}")
+async def add(num1: float, num2: float):
+    return {"message": f"Hello {num1+num2}"}
