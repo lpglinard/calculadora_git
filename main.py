8bc779dbd74d56a55b1357a272b30b6da9498e14
F6323258 Lucas Pereira Gomes Linard <lucaslinard@bb.com.br>
2024-06-18 08:43:13 -0300

    Sabrina Santos

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/subtract/{num1}")
+async def subtract(num1: str):
+    return {"message": f"Hello {num1}"}
